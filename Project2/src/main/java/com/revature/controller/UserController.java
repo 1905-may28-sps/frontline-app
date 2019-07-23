ca24563e9a09e323f6d92ca564b6494512e4bb6c
DHutson97 <blankempty97@gmail.com>
2019-07-23 12:49:14 -0400

    updated better fixed user

--- Project2/src/main/java/com/revature/controller/UserController.java
+++ Project2/src/main/java/com/revature/controller/UserController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.revature.beans.User;
 import com.revature.service.UserService;
 
+@Controller
 public class UserController {
 	@Autowired
 	private UserService service;
